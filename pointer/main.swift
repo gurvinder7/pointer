24f0ac8db7585e88ebc7dbfbe9c71468d1c0d64c
gurvinder mangat <gurvindermangat@gurvinders-MacBook-Pro.local>
2019-02-19 18:36:48 -0500

    Initial Commit

---
+++ pointer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pointer
+//
+//  Created by gurvinder mangat on 2019-02-19.
+//  Copyright © 2019 gurvinder mangat. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
